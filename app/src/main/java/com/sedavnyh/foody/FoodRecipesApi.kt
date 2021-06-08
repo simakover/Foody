54d09e99da4cfc01aadb4748ef8aedd86362bdc9
Simakover <Simakover@gmail.com>
2021-06-08 16:09:37 +0500

    API interface and get function getRecipes

---
+++ app/src/main/java/com/sedavnyh/foody/FoodRecipesApi.kt
@@ -0,0 +1,15 @@
+package com.sedavnyh.foody
+
+import com.sedavnyh.foody.models.FoodRecipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface FoodRecipesApi {
+
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipes(
+        @QueryMap queries: Map<String, String>
+    ): Response<FoodRecipe>
+
+}
