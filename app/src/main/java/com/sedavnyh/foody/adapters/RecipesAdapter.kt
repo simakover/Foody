2ac40047c44922345c446d1199a707c576a85168
Simakover <Simakover@gmail.com>
2021-06-11 15:43:39 +0500

    Binding adapter for row, containts changes to textView and ImageView dynamic

--- app/src/main/java/com/sedavnyh/foody/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/sedavnyh/foody/adapters/RecipesAdapter.kt
@@ -2,18 +2,20 @@ package com.sedavnyh.foody.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sedavnyh.foody.databinding.RecipesRowLayoutBinding
 import com.sedavnyh.foody.models.FoodRecipe
 import com.sedavnyh.foody.models.Result
+import com.sedavnyh.foody.util.RecipesDiffUtil
 
 
 // Байндинг адаптер для списка рецептов рецепта. Создаем адаптер, как расширение адаптера Ресейклер вью
-// Прокидываем кастомный вьюхолдер
+// Прокидываем кастомный вьюхолдер, привязывает данные из апи к конкретным карточкам
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
 
     // Список рецептов
-    private var recipe = emptyList<Result>()
+    private var recipes = emptyList<Result>()
 
     // Создаем кастомный вьюхолдер, прокидываем байдинг Recipes_row.xml
     class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
@@ -35,24 +37,27 @@ class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
             }
     }
 
-    // При создании вьюхолдера запускает функцию для привязки
+    // При создании вьюхолдера запускает функцию для создания и привязки
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         return MyViewHolder.from(parent)
     }
 
     // Привязывает конкретный рецепт к вьюхолдеру
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentResult = recipe[position]
-        holder.bind(currentResult)
+        val currentRecipe = recipes[position]
+        holder.bind(currentRecipe)
     }
 
     override fun getItemCount(): Int {
-        return recipe.size
+        return recipes.size
     }
 
     // Записывает все рецепты из модели(из апи) в переменную рецептов
+    // Сравнивает новый и старый спиок, обновляет только при наличии изменений
     fun setData(newData: FoodRecipe){
-        recipe = newData.results
-        notifyDataSetChanged()
+        val recipesDiffUtil = RecipesDiffUtil(recipes, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipes = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
     }
 }
---
+++ app/src/main/java/com/sedavnyh/foody/bindingadapters/RecipesRowBinding.kt
@@ -0,0 +1,47 @@
+package com.sedavnyh.foody.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.sedavnyh.foody.R
+
+// Байндинг адапетр для конкретного рецетпа\карточки
+class RecipesRowBinding {
+
+    // объект для обращения к классу без создания екземпляров
+    companion object{
+
+        // Создаем новый параметр для вью который устанавлиет текст вью
+        // JvmStatic дает доступ из любого места
+        // BindingAdapter - создает свойство для вью в лейауте
+        // первый параметр обязательный - тип вью
+        @BindingAdapter("setNumberOfLikes")
+        @JvmStatic
+        fun setNumberOfLikes(textView: TextView, likes: Int){
+            textView.text = likes.toString()
+        }
+
+        @BindingAdapter("setNumberOfMinutes")
+        @JvmStatic
+        fun setNumberOfMinutes(textView: TextView, minutes: Int){
+            textView.text = minutes.toString()
+        }
+
+        // Установить цвет взависимости от веган\не веган
+        // Если вью - текстовое, то цвет текста
+        // Если вью - картинка, то тинт
+        @BindingAdapter("isVegan")
+        @JvmStatic
+        fun isVegan(view: View, vegan: Boolean){
+            if (vegan){
+                when(view){
+                    is TextView -> view.setTextColor(ContextCompat.getColor(view.context, R.color.green))
+                    is ImageView -> view.setColorFilter(ContextCompat.getColor(view.context, R.color.green))
+                }
+            }
+
+        }
+    }
+}
