28a297a37172d86c5b4c615e5ed2ad7245d64846
Simakover <Simakover@gmail.com>
2021-06-15 17:45:44 +0500

    type converters

---
+++ app/src/main/java/com/sedavnyh/foody/RecipesTypeConverter.kt
@@ -0,0 +1,24 @@
+package com.sedavnyh.foody
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.sedavnyh.foody.models.FoodRecipe
+
+// конвертация сущности списка рецептов для записи в таблицу
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+}
