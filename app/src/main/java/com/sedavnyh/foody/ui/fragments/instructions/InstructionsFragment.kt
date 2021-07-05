65c3f221b28a56f6d45983050ead61c7fd46aec1
Simakover <Simakover@gmail.com>
2021-07-05 21:08:04 +0500

    implemented webView for InstructionsFragment

--- app/src/main/java/com/sedavnyh/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/sedavnyh/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,13 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.sedavnyh.foody.R
+import com.sedavnyh.foody.models.Result
+import com.sedavnyh.foody.util.Constants.Companion.RECIPE_RESULT_KEY
+import kotlinx.android.synthetic.main.fragment_instructions.view.*
 
 class InstructionsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view =  inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        // загрузка аргументов из бандла
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(RECIPE_RESULT_KEY)
+
+        // определение для webView
+        view.instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl = myBundle!!.sourceUrl
+        view.instructions_webView.loadUrl(websiteUrl)
+
+        return view
     }
 }
