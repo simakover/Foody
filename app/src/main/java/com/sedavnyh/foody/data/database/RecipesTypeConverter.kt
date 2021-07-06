de5ed5dccde02097abc79e48b1604e6d588826b7
Simakover <Simakover@gmail.com>
2021-07-06 16:01:51 +0500

    type converters for Result entity

--- app/src/main/java/com/sedavnyh/foody/data/database/RecipesTypeConverter.kt
+++ app/src/main/java/com/sedavnyh/foody/data/database/RecipesTypeConverter.kt
@@ -4,12 +4,15 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.sedavnyh.foody.models.FoodRecipe
+import com.sedavnyh.foody.models.Result
 
-// конвертация сущности списка рецептов для записи в таблицу
+// конвертация сущностей
 class RecipesTypeConverter {
 
     var gson = Gson()
 
+
+    //Конвертации сущности списка рецептов
     @TypeConverter
     fun foodRecipeToString(foodRecipe: FoodRecipe): String {
         return gson.toJson(foodRecipe)
@@ -19,6 +22,18 @@ class RecipesTypeConverter {
     fun stringToFoodRecipe(data: String): FoodRecipe {
         val listType = object : TypeToken<FoodRecipe>() {}.type
         return gson.fromJson(data, listType)
+    }
+
+    //Конвертации сущности избранного рецепта
+    @TypeConverter
+    fun resultToString(result : Result): String {
+        return gson.toJson(result)
+    }
+
+    @TypeConverter
+    fun stringToResult(data: String): Result {
+        val type = object : TypeToken<Result>() {}.type
+        return gson.fromJson(data, type)
     }
 
 }
--- app/src/main/java/com/sedavnyh/foody/data/database/entities/FavoritesEntity.kt
+++ app/src/main/java/com/sedavnyh/foody/data/database/entities/FavoritesEntity.kt
@@ -5,6 +5,7 @@ import androidx.room.PrimaryKey
 import com.sedavnyh.foody.models.Result
 import com.sedavnyh.foody.util.Constants.Companion.FAVORITE_RECIPES_TABLE
 
+// Сущность для хранения избранных рецептов
 @Entity(tableName = FAVORITE_RECIPES_TABLE)
 class FavoritesEntity(
     @PrimaryKey(autoGenerate = true)
