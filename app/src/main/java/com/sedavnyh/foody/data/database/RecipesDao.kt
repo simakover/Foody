aa9d7aee711608bb4ba140f0f416ec31892cbba5
Simakover <Simakover@gmail.com>
2021-07-06 16:10:42 +0500

    modified DAO (queries) for working with favorite recipes

--- app/src/main/java/com/sedavnyh/foody/data/database/RecipesDao.kt
+++ app/src/main/java/com/sedavnyh/foody/data/database/RecipesDao.kt
@@ -1,9 +1,7 @@
 package com.sedavnyh.foody.data.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.sedavnyh.foody.data.database.entities.FavoritesEntity
 import com.sedavnyh.foody.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -16,4 +14,18 @@ interface RecipesDao {
 
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+    // Запись, удаление и чтение данных по избранным рецептам
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
+
 }
