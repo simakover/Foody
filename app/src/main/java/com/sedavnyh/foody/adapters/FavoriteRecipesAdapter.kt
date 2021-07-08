9eaccace2e4778218e258457e5f716b38b425143
Simakover <Simakover@gmail.com>
2021-07-08 21:29:17 +0500

    bind FavoriteRecipesAdapter and FavoriteRecipesFragment

---
+++ app/src/main/java/com/sedavnyh/foody/adapters/FavoriteRecipesAdapter.kt
@@ -0,0 +1,55 @@
+package com.sedavnyh.foody.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.sedavnyh.foody.data.database.entities.FavoritesEntity
+import com.sedavnyh.foody.databinding.FavoriteRecipesRowLayoutBinding
+import com.sedavnyh.foody.util.RecipesDiffUtil
+
+// Байндинг адаптер для списка избраных рецептов рецептов. Создаем адаптер, как расширение адаптера Ресейклер вью
+// Прокидываем кастомный вьюхолдер, привязывает данные
+class FavoriteRecipesAdapter: RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+
+    private var favoriteRecipes = emptyList<FavoritesEntity>()
+
+    class MyViewHolder(private val binding: FavoriteRecipesRowLayoutBinding): RecyclerView.ViewHolder(binding.root) {
+
+        // Привязываем данные к переменной в лейауте
+        fun bind(favoritesEntity: FavoritesEntity) {
+            binding.favoritesEntity = favoritesEntity
+            binding.executePendingBindings()
+        }
+
+        // Связываем холдер и лейаут строки избраного рецепта
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = FavoriteRecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val selectedRecipe = favoriteRecipes[position]
+        holder.bind(selectedRecipe)
+    }
+
+    override fun getItemCount(): Int {
+        return favoriteRecipes.size
+    }
+
+    fun setData(newFavoriteRecipes: List<FavoritesEntity>) {
+        val favoriteRecipesDiffUtil = RecipesDiffUtil(favoriteRecipes, newFavoriteRecipes)
+        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
+        favoriteRecipes = newFavoriteRecipes
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
--- app/src/main/java/com/sedavnyh/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/sedavnyh/foody/ui/fragments/favorites/FavoriteRecipesFragment.kt
@@ -5,16 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sedavnyh.foody.R
+import com.sedavnyh.foody.adapters.FavoriteRecipesAdapter
+import com.sedavnyh.foody.databinding.FragmentFavoriteRecipesBinding
+import com.sedavnyh.foody.databinding.FragmentIngredientsBinding
+import com.sedavnyh.foody.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteRecipesFragment : Fragment() {
+
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+    private val mAdapter by lazy { FavoriteRecipesAdapter()  }
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        _binding = FragmentFavoriteRecipesBinding.inflate(inflater, container, false)
+
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner, {
+            mAdapter.setData(it)
+        })
+
+        setupRecyclerView()
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.favoriteRecipesRecyclerView.adapter = mAdapter
+        binding.favoriteRecipesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
 }
