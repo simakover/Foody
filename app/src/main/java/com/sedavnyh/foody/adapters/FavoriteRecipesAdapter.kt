d06f517515ad9f17cdd2ac83f2e0c5693749e5c7
Simakover <Simakover@gmail.com>
2021-07-17 00:28:50 +0500

    multiselection

--- app/src/main/java/com/sedavnyh/foody/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/sedavnyh/foody/adapters/FavoriteRecipesAdapter.kt
@@ -21,6 +21,10 @@ class FavoriteRecipesAdapter(
 
     private var favoriteRecipes = emptyList<FavoritesEntity>()
 
+    private var multiSelection: Boolean = false
+    private var myViewHolders = arrayListOf<MyViewHolder>()
+    private var selectedRecipes = arrayListOf<FavoritesEntity>()
+
     class MyViewHolder(private val binding: FavoriteRecipesRowLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
 
         // Привязываем данные к переменной в лейауте
@@ -45,21 +49,32 @@ class FavoriteRecipesAdapter(
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val selectedRecipe = favoriteRecipes[position]
-        holder.bind(selectedRecipe)
-
+        val currentRecipe = favoriteRecipes[position]
+        holder.bind(currentRecipe)
+        myViewHolders.add(holder)
 
         // single click listener
         holder.itemView.favoriteRecipesRowLayout.setOnClickListener {
-            val action =
-                FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(selectedRecipe.result)
-            holder.itemView.findNavController().navigate(action)
+            if (multiSelection) {
+                applySelection(holder, currentRecipe)
+            } else {
+                val action =
+                    FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailsActivity(currentRecipe.result)
+                holder.itemView.findNavController().navigate(action)
+            }
         }
 
         // long click listener
         holder.itemView.favoriteRecipesRowLayout.setOnLongClickListener {
-            requireActivity.startActionMode(this)
-            true
+            if (!multiSelection) {
+                multiSelection = true
+                requireActivity.startActionMode(this)
+                applySelection(holder, currentRecipe)
+                true
+            } else {
+                multiSelection = false
+                false
+            }
         }
     }
 
@@ -72,6 +87,31 @@ class FavoriteRecipesAdapter(
         val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
         favoriteRecipes = newFavoriteRecipes
         diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+    private fun applySelection(holder: MyViewHolder, currentRecipe: FavoritesEntity) {
+        if (selectedRecipes.contains(currentRecipe)) {
+            selectedRecipes.remove(currentRecipe)
+            changeRecipeStyle(holder, R.color.cardBackgroundColor, R.color.strokeColor)
+        } else {
+            selectedRecipes.add(currentRecipe)
+            changeRecipeStyle(holder, R.color.cardBackgroundLightColor, R.color.colorPrimary)
+        }
+    }
+
+    private fun changeRecipeStyle(holder: MyViewHolder, backgroundColor: Int, strokeColor: Int) {
+        holder.itemView.favoriteRecipesRowLayout.setBackgroundColor(
+            ContextCompat.getColor(
+                requireActivity,
+                backgroundColor
+            )
+        )
+        holder.itemView.favorite_row_cardView.strokeColor = ContextCompat.getColor(requireActivity, strokeColor)
+    }
+
+    // Смена цвета статус бара
+    private fun applyStatusBarColor(color: Int) {
+        requireActivity.window.statusBarColor = ContextCompat.getColor(requireActivity, color)
     }
 
     // Переход в режим выбора по лонг клику
@@ -90,11 +130,13 @@ class FavoriteRecipesAdapter(
     }
 
     override fun onDestroyActionMode(mode: ActionMode?) {
+        multiSelection = false
+        selectedRecipes.clear()
         applyStatusBarColor(R.color.statusBarColor)
+        myViewHolders.forEach {
+            changeRecipeStyle(it, R.color.cardBackgroundColor, R.color.strokeColor)
+        }
     }
 
-    // Смена цвета статус бара
-    private fun applyStatusBarColor(color: Int) {
-        requireActivity.window.statusBarColor = ContextCompat.getColor(requireActivity, color)
-    }
+
 }
